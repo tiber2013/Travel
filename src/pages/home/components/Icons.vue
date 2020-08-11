12f07d449b8b7a97957b5fa672d097ad3c8fef26
QCChou <tiberium@live.cn>
2020-08-11 12:04:37 +0800

    add Recommend

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -83,38 +83,34 @@ export default {
       height: 0
       padding-bottom: 50%
     .icons
-      overflow: hidden
-      height: 0
-      padding-bottom: 50%
-      background: lightgreen
-    .icon
-        position: relative
-        overflow: hidden
-        float: left
-        width: 25%
-        height: 0
-        padding-bottom: 25%
-        background: lightblue
-        .icon-img
-          position: absolute
-          top: 0
-          left: 0
-          right: 0
-          bottom: .44rem
-          box-sizing: border-box
-          padding: .1rem
-          .icon-img-content
-            display: block
-            margin: 0 auto
-            height: 100%
-        .icon-desc
-          position: absolute
-          left: 0
-          right: 0
-          bottom: 0
-          height: .44rem
-          line-height: .44rem
-          text-align: center
-          color: $darkTextColor
-          ellipsis()
+      margin-top:.1rem
+      .icon
+          position: relative
+          overflow: hidden
+          float: left
+          width: 25%
+          height: 0
+          padding-bottom: 25%
+          .icon-img
+            position: absolute
+            top: 0
+            left: 0
+            right: 0
+            bottom: .44rem
+            box-sizing: border-box
+            padding: .1rem
+            .icon-img-content
+              display: block
+              margin: 0 auto
+              height: 100%
+          .icon-desc
+            position: absolute
+            left: 0
+            right: 0
+            bottom: 0
+            height: .44rem
+            line-height: .44rem
+            text-align: center
+            color: $darkTextColor
+            ellipsis()
 </style>
