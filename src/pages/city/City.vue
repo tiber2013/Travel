109c1e210e694a0c9ede1119f5921f5174c6e0f6
QCChou <tiberium@live.cn>
2020-08-13 22:38:25 +0800

    finish citylist

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,23 @@
   <div class="">
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/CityHeader.vue'
 import CitySearch from './components/CitySearch.vue'
+import CityList from './components/CityList.vue'
+import CityAlphabet from './components/Alphabet.vue'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
