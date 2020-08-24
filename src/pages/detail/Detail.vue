4c744600843c469ace8ad00688b18e85067fb3e8
QCChou <tiberium@live.cn>
2020-08-24 15:13:24 +0800

    detail list and ajax

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,53 @@
 <div class="detail">
   <detail-banner></detail-banner>
   <detail-header></detail-header>
-  <div class="content"></div>
+  <div class="content">
+    <detail-list :list="list"></detail-list>
+  </div>
 </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner2'
 import DetailHeader from './components/Header.vue'
+import DetailList from './components/List'
+import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      list: []
+    }
+  },
+  methods: {
+    getDetailInfo () {
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.sightName = data.sightName
+        this.bannerImg = data.bannerImg
+        this.gallaryImgs = data.gallaryImgs
+        this.list = data.categoryList
+      }
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
   }
 }
 </script>
